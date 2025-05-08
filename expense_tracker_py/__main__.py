d2664a7e5be68f23fe9c795abb03d41a8e896535
Mulih <almulih@yahoo.com>
2025-05-08 09:13:25 +0300

    Refactor main function to enhance CLI command handling and add argument parsing for new expense

--- expense_tracker_py/__main__.py
+++ expense_tracker_py/__main__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 import os
 from expense import *
 
@@ -8,4 +8,23 @@ def main():
     parser = ArgumentParser(description="Expense Tracker CLI")
     subparsers = parser.add_subparsers(dest="command", help="Commands")
 
-    
+    add_parser = subparsers.add_parser("add", help="A new expense")
+    add_parser.add_argument("--description",type=str, required=True, help="Expense description")
+    add_parser.add_argument("--amount", type=float, required=True, help="Expense amount")
+
+    args: Namespace = parser.parse_args()
+
+    if not args.command:
+        parser.print_help()
+        return
+
+    commands = {
+        "add": lambda: add_expense(args.description, args.amount),
+    }
+
+    command_function = commands.get(args.command, parser.print_help)
+    command_function()
+
+
+if __name__ == "__main__":
+    main()
